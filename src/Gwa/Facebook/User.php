7ed709a6b480b0866902c183ae287f736fd17f66
Timothy Groves <groves@greatwhiteark.com>
2015-01-07 14:33:55 +0100

    Initial commit

---
+++ src/Gwa/Facebook/User.php
@@ -0,0 +1,170 @@
+<?php
+namespace Gwa\Facebook;
+
+use Gwa\Filesystem\gwDirectory;
+use Gwa\Filesystem\gwFile;
+
+use Gwa\Remote\RemoteResource;
+
+use Facebook\FacebookRequest;
+use Facebook\FacebookSession;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+
+/**
+ * @brief Singleton class representing the current facebook user.
+ *
+ * Config parameters:
+ * - fb_app_id
+ * - fb_app_key
+ *
+ * @ingroup facebook
+ */
+class User
+{
+    private $_appid;
+
+    /**
+     * @var Facebook
+     */
+    private $_session;
+
+    /**
+     * @var gwRequest
+     */
+    private $_request;
+
+    /**
+     * @var array
+     */
+    private $_requestdata;
+
+    /**
+     * @var GraphUser
+     */
+    private $_me;
+
+    /* ---------------------------------------------------------------- */
+
+    public function __construct( $appid, $appsecret )
+    {
+        $this->_appid = $appid;
+        FacebookSession::setDefaultApplication($appid, $appsecret);
+    }
+
+    /**
+     * @brief Parses the signed request passed by facebook.
+     */
+    public function setSignedRequest( $signedrequest )
+    {
+        $this->_session = FacebookSession::newSessionFromSignedRequest($signedrequest);
+        $this->_session->validate();
+        return $this;
+    }
+
+    /**
+     * @brief Sets the user access token.
+     * @return User
+     */
+    public function setAccessToken( $token )
+    {
+        $this->_session = new FacebookSession($token);
+        $this->_session->validate();
+        return $this;
+    }
+
+    /**
+     * @brief Sets the app access token.
+     * @return User
+     */
+    public function setAppAccessToken()
+    {
+        $this->_session = FacebookSession::newAppSession();
+        $this->_session->validate();
+        return $this;
+    }
+
+    /**
+     * @param  string        $path
+     * @param  string        $method
+     * @param  array|null    $params
+     * @param  string|null   $version
+     * @return Facebook\FacebookRequest
+     */
+    public function createRequest( $path, $method = 'get', $params = null, $version = null )
+    {
+        return new FacebookRequest(
+            $this->_session,
+            $method,
+            $path,
+            $params,
+            $version
+        );
+    }
+
+    /**
+     * @brief Save current users photo to local asset folder.
+     *
+     * Useful for caching user photos locally.
+     *
+     * @param string $folderpath
+     * @param string $userid
+     * @param string $type square | small | normal | large
+     * @param string $filename
+     * @return string path to saved file | NULL
+     */
+    public function savePictureLocally( $folderpath, $userid = null, $type = 'normal', $filename = null  )
+    {
+        if (!$userid) {
+            $userid = $this->me()->getId();
+        }
+
+        $dir = gwDirectory::makeDirectoryRecursive($folderpath);
+        $url = 'https://graph.facebook.com/' . $userid . '/picture';
+
+        if ($type) {
+            $url .= '?type=' . $type;
+        }
+
+        $page = new RemoteResource($url);
+        if ($page->fetch()) {
+            $headers = $page->getHeaders();
+            $url = $headers['url'];
+            $data = file_get_contents($url);
+            if (!$filename) {
+                $filename = $userid;
+            }
+            $path = $folderpath . '/' . $filename . '.jpg';
+
+            $localfile = new gwFile($path);
+            $localfile->replaceContent($data);
+
+            return $path;
+        }
+
+        return null;
+    }
+
+    /**
+     * @brief Returns an array of user data retrieved via the FB api '/me' call.
+     * @link https://graph.facebook.com/btaylor
+     * @return Facebook\GraphUser
+     */
+    public function me()
+    {
+        if (!isset($this->_me)) {
+            $this->_me = (new FacebookRequest(
+                $this->_session, 'GET', '/me'
+            ))->execute()->getGraphObject(GraphUser::className());
+        }
+        return $this->_me;
+    }
+
+    /**
+     * @brief Unsets the "me" instance.
+     */
+    public function unsetMe()
+    {
+        $this->_me = null;
+    }
+}
---
+++ tests/UserTest.php
@@ -0,0 +1,55 @@
+<?php
+use Gwa\Facebook\User;
+use Gwa\Filesystem\gwDirectory;
+
+class UserTest extends PHPUnit_Framework_TestCase
+{
+    const APP_ID       = '383320168496795';
+    const APP_SECRET   = '867cb607898cb0b84125df45e3bcf331';
+    const ACCESS_TOKEN = 'CAAFcoK56fpsBAKtjx6YaYOmmNd5XpwhqaqU8kY9gIabdXzXaXkpeYcZBq8l6piESoqvdxXZASWtNjGPmsmjv4RZADziNZA5XXOt8RTaA4ufbtTmqg1ucuwFi2sCejkD05mU3qLvF5UrMkZCJuwZAFeoQGsHttf9J9JA89KxzhEKdE7TJKVPPirZB40C2sXXliuOszxZBnG8IR9bqCzy6ZAWxcgpYstJSi5HQZD';
+
+    const TEST_USER_ID = '1386357951666752';
+
+    static public function setUpBeforeClass()
+    {
+        $dir = new gwDirectory(__DIR__ . '/images');
+        $dir->emptyDirectory();
+    }
+
+    public function testCanBeConstructed()
+    {
+        $user = new User(self::APP_ID, self::APP_SECRET);
+        $this->assertInstanceOf('Gwa\Facebook\User', $user);
+    }
+
+    public function testCanSetAccessToken()
+    {
+        $user = new User(self::APP_ID, self::APP_SECRET);
+        $user->setAccessToken(self::ACCESS_TOKEN);
+        $me = $user->me();
+        $this->assertInstanceOf('Facebook\GraphUser', $me);
+        $this->assertEquals(self::TEST_USER_ID, $me->getId());
+    }
+
+    public function testCanSavePictureLocally()
+    {
+        $user = new User(self::APP_ID, self::APP_SECRET);
+        $user->savePictureLocally(
+            __DIR__ . '/images',
+            '562803288'
+        );
+        $this->assertTrue(file_exists(__DIR__ . '/images/562803288.jpg'));
+
+        $user->savePictureLocally(
+            __DIR__ . '/images',
+            '562803288',
+            'large',
+            '562803288_large'
+        );
+        $this->assertTrue(file_exists(__DIR__ . '/images/562803288_large.jpg'));
+
+        $user->setAccessToken(self::ACCESS_TOKEN);
+        $user->savePictureLocally(__DIR__ . '/images');
+        $this->assertTrue(file_exists(__DIR__ . '/images/' . self::TEST_USER_ID . '.jpg'));
+    }
+}
